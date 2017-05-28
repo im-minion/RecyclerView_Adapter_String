ed025a8aaffa7a8febe03583b7e249f737a840ad
im-minion <vaibhavminiyar@gmail.com>
2017-05-28 23:22:17 +0530

    removed

--- app/src/main/java/in/vaibhav/com/recyclerviewadapter/MainActivity.java
+++ app/src/main/java/in/vaibhav/com/recyclerviewadapter/MainActivity.java
@@ -20,24 +20,5 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(new Adapter(this,Users.getUsers()));
 
-
-        recyclerView.addOnItemTouchListener(new RecyclerView.OnItemTouchListener() {
-            @Override
-            public boolean onInterceptTouchEvent(RecyclerView rv, MotionEvent e) {
-//                Toast.makeText(MainActivity.this,"TAG" + rv.,Toast.LENGTH_SHORT).show();
-
-                return false;
-            }
-
-            @Override
-            public void onTouchEvent(RecyclerView rv, MotionEvent e) {
-
-            }
-
-            @Override
-            public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {
-
-            }
-        });
     }
 }
